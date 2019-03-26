114a83e0e93f7b53234d7eff3e5ca22be8f60088
Miguel Barbosa <miguelbarbosa1309@gmail.com>
2019-03-26 16:42:48 -0300

    Mudei 26/03

--- js/exemplo20.js
+++ js/exemplo20.js
@@ -1,28 +1,38 @@
-function limpar(){
-    let nome = document.getElementById("nome");
-    nome.value = "";
-    nome.focus();
+function qtd_linhas() {
+    let funcionarios = document.getElementById("tabela-funcionarios");
+    let qtd = funcionarios.getElementsByTagName("tr").length;
+    return qtd;
+}
 
-    let idade = document.getElementById("idade");
-    idade.value = "18";
+function limpar_formulario() {
+    document.getElementById("nome").value = "";
+    document.getElementById("idade").value = 18;
+    document.getElementById("masculino").checked = true;
+    document.getElementById("nome").focus;
+}
+
+function inserir_funcionario() {
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value;
+    let sexo = "a definir";
+    if (document.getElementById("masculino").checked) {
+        sexo = "Masculino";
+    } else {
+        sexo = "Feminino";
+    }
+    adicionar_tabela(nome, sexo, idade);
+    limpar_formulario();
+}
+
+function adicionar_tabela(nome, sexo, idade) {
+    let tabela = document.getElementById("tabela-funcionarios");
+    let linha = tabela.insertRow(qtd_linhas());
     
+    let td_nome = linha.insertCell(0);
+    let td_sexo = linha.insertCell(1);
+    let td_idade = linha.insertCell(2);
 
-
+    td_nome.innerHTML = nome;
+    td_sexo.innerHTML = sexo;
+    td_idade.innerHTML = idade;
 }
-
-
-function salvar(){
-    let nome = document.getElementById("nome");
-    let txt = document.getElementsByTagName("td");
-    txt[0].innerHTML = nome.value;
-
-
-    let idade = document.getElementById("idade");
-    txt[2].innerHTML = idade.value;
-
-}
-
-// let paragrafos = document.getElementsByTagName("p");
-
-// paragrafos[0].innerHTML = "Paragrafo modificado";
-// alert(paragrafos[0].innerHTML);
