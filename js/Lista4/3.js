29d380eeea1874b50b49141b330d367d66b19bb2
Miguel Barbosa <miguelbarbosa1309@gmail.com>
2019-03-29 16:34:00 -0300

    mudei 29/03

---
+++ js/Lista4/3.js
@@ -0,0 +1,36 @@
+let num = parseFloat(prompt("Digite um número: "));
+
+switch (num){
+    case 1 :
+    alert("Domingo");
+    break;
+
+    case 2 :
+    alert("Segunda");
+    break;
+
+    case 3 :
+    alert("Terça");
+    break;
+
+    case 4 :
+    alert("Quarta");
+    break;
+
+    case 5 :
+    alert("Quinta");
+    break;
+
+    case 6 :
+    alert("Sexta");
+    break;
+
+    case 7 :
+    alert("Sábado");
+    break;
+
+    default:
+    alert("Valor Inválido");
+    break;
+
+}
