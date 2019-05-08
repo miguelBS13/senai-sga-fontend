2c7c33e67efe88e0dab878cdc565e5f8391e9df6
Miguel Barbosa <miguelbarbosa1309@gmail.com>
2019-05-08 16:56:03 -0300

    mudei 08/05

---
+++ projeto1/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
+
+class Cliente{
+  nome: string;
+  email: string;
+  profissao = '';
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  cliente = new Cliente();
+  profissoes = ['Programador', 'Empresário', 'Outra'];
+
+  salvar(form: NgForm) {
+    console.log(form.value);
+    console.log(form);
+
+    form.reset({profissao: ''});
+  }
+}
